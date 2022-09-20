c8710520e339b6e53556616db919d87f6f70140c
thewoowon <thewoowon@naver.com>
2022-09-20 16:46:45 +0900

    20220920_SALAD_PEACE

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { BuildingsModule } from './buildings/buildings.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
+import { PaymentsModule } from './payments/payments.module';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -11,6 +19,29 @@ import { BuildingsModule } from './buildings/buildings.module';
       autoSchemaFile: true,
     }),
     BuildingsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      ...(process.env.DATABASE_URL
+        ? { url: process.env.DATABASE_URL }
+        : {
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+          }),
+      synchronize: true,
+      logging:
+        process.env.NODE_ENV !== 'production' &&
+        process.env.NODE_ENV !== 'test',
+    }),
+    UsersModule,
+    OrdersModule,
+    PaymentsModule,
+    MailModule,
+    AuthModule,
+    CommonModule,
+    JwtModule,
   ],
   controllers: [],
   providers: [],
